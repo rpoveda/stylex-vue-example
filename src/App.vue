a4228520db3cd48abe0a09e006de5b764028a16a
Rafael Poveda <raffael.poveda@gmail.com>
2023-12-14 08:33:22 -0300

    Commit to create project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+
+// import HelloWorld from './components/HelloWorld.vue'
+
+import { ref, onMounted } from 'vue'
+import * as stylex from "@stylexjs/stylex";
+import { buttons } from './stylex/button-stylex'
+
+onMounted(() => {
+  
+})
+</script>
+
+<template>
+  <div>
+      <button :class=stylex.props(buttons.primary).className>
+        Enviar
+      </button>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/stylex/button-stylex.ts
@@ -0,0 +1,9 @@
+import * as stylex from "@stylexjs/stylex";
+
+export const buttons = stylex.create({
+  primary: {
+    backgroundColor: "blue",
+    color: "white",
+    fontSize: 26
+  },
+});
